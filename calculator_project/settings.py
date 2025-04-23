8bb593567e233fa55efec5dcfcfe5392f35df00e
Harshith <84128979+harshithvulisetti@users.noreply.github.com>
2025-04-23 11:49:09 +0530

    Update settings.py

--- calculator_project/settings.py
+++ calculator_project/settings.py
@@ -6,7 +6,7 @@ SECRET_KEY = 'django-insecure-your-secret-key-here'
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-d385.onrender.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'django-d385.onrender.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
